de9bd82507e45c8f75e45e160b8f4bbda43dbdf1
gregrory <gregrory>
2017-05-19 17:22:52 +0800

    第二次测试

--- spec/main_test.js
+++ spec/main_test.js
@@ -15,13 +15,29 @@ var main = require("../testmain/main.js");
 describe("测试描述",function () {
     sinon.spy(console, 'log');
 
-    it ("传入'he' 返回‘he 1’", function () {
+    it("传入'he' 返回‘he 1’", function () {
 
         var result = main('he');
         var expect_string = 'he 1';
 
         expect(expect_string).to.equal(result);
 
-    })
+    });
+    it("传入'he is' 返回‘he 1\nis 1’", function () {
+
+        var result = main('he is');
+        var expect_string = 'he 1\nis 1';
+
+        expect(expect_string).to.equal(result);
+
+    });
+    it("传入'he    is    ' 返回‘he 1\nis 1’", function () {
+
+        var result = main('   he    is    ');
+        var expect_string = 'he 1\nis 1';
+
+        expect(expect_string).to.equal(result);
+
+    });
 
 })
--- testmain/main.js
+++ testmain/main.js
@@ -2,8 +2,12 @@
  * Created by gregrory on 17-5-18.
  */
 function main(string) {
+    var words = string.trim().split(/\s+/);
 
-    return string+' ' + 1;
+    if (words.length === 1 )
+        return words[0] + ' ' + 1;
+    else if(words.length === 2){
+        return words[0] + ' ' + 1 + '\n' + words[1] + ' ' + 1 ;
+    }
 }
-
 module.exports = main;
