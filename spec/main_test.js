fb8fe4d1830db59e3aed3c46e22ba754908bf932
gregrory <gregrory>
2017-05-19 18:42:46 +0800

    完成

--- spec/main_test.js
+++ spec/main_test.js
@@ -39,5 +39,20 @@ describe("测试描述",function () {
         expect(expect_string).to.equal(result);
 
     });
+    it("传入'he    he  is ' 返回‘he 2\nis 1’", function () {
 
-})
+        var result = main('   he    he  is   ');
+        var expect_string = 'he 2\nis 1';
+
+        expect(expect_string).to.equal(result);
+
+    });
+    it("传入'he is a he he is is is' 返回‘is 4\nhe 3\na 1’", function () {
+
+        var result = main('he is a he he is is is');
+        var expect_string = 'is 4\nhe 3\na 1';
+
+        expect(expect_string).to.equal(result);
+
+    });
+});
--- testmain/main.js
+++ testmain/main.js
@@ -3,11 +3,65 @@
  */
 function main(string) {
     var words = string.trim().split(/\s+/);
+    var splitWords = [];
+    var num = [];
 
-    if (words.length === 1 )
-        return words[0] + ' ' + 1;
-    else if(words.length === 2){
-        return words[0] + ' ' + 1 + '\n' + words[1] + ' ' + 1 ;
+    function findPositon(arr, w) {
+        var ret = -1;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === w) {
+                ret = i;
+            }
+        }
+        return ret;
     }
+
+    function classify(words,num) {
+        for (var i = 0; i < words.length; i++) {
+            var pos = findPositon(splitWords, words[i]);
+            if (pos !== -1) {
+                num[pos]++;
+            } else {
+
+                splitWords.push(words[i]);
+                num.push(1);
+            }
+        }
+    }
+
+    function format(splitWords,num) {
+        var answer = '';
+        for (var i = 0; i < splitWords.length; i++) {
+            answer += splitWords[i] + ' ' + num[i];
+            if (i !== splitWords.length - 1) {
+                answer += '\n';
+            }
+        }
+
+        return answer;
+    }
+
+    function SortWords(splitWords,num) {
+        for (var i = 0; i < num.length; i++) {
+            for (var j = i; j < num.length; j++) {
+                if (num[i] < num[j]) {
+                    var temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                    temp = splitWords[i];
+                    splitWords[i] = splitWords[j];
+                    splitWords[j] = temp;
+                }
+            }
+        }
+    }
+
+    classify(words,num);
+
+    SortWords(splitWords,num);
+
+    return format(splitWords,num);
 }
+
+
 module.exports = main;
